2449b50213ee798a1629271789885d979fb9cbac
limunosekai <powerlsh0103@gmail.com>
2020-11-12 00:15:32 +0900

    Add toggle button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
-const moreBtn = document.querySelector('info .metadata .moreBtn');
-const title = document.querySelector('.info .metadata .title');
+const moreBtn = document.querySelector('info .metadata .titleAndButton .moreBtn');
+const title = document.querySelector('.info .metadata .titleAndButton .title clamp');
 
 moreBtn.addEventListener('click', () => {
     moreBtn.classList.toggle('clicked');
     title.classList.toggle('clamp');
+    console.log(1);
 });
