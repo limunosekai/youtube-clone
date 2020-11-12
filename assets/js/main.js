f02ca5a60b5f60c03e1390736be9e39ea98f7639
limunosekai <powerlsh0103@gmail.com>
2020-11-12 23:15:32 +0900

    Final version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,7 @@
-const moreBtn = document.querySelector('info .metadata .titleAndButton .moreBtn');
-const title = document.querySelector('.info .metadata .titleAndButton .title clamp');
+const moreBtn = document.querySelector('info .metadata .moreBtn');
+const title = document.querySelector('.info .metadata .title');
 
 moreBtn.addEventListener('click', () => {
     moreBtn.classList.toggle('clicked');
     title.classList.toggle('clamp');
-    console.log(1);
 });
